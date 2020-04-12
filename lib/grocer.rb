b8af5129959aaafa6eb09982da7df3f87d17a31b
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-12 19:30:31 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -8,11 +8,10 @@ def find_item_by_name_in_collection(name, collection)
   collection.each do | item |
     if collection[count][:item] == name 
       return collection[count]
-    else
-      return "nil"
     end
     count += 1
   end
+  return "nil"
 end
 
 def consolidate_cart(cart)
