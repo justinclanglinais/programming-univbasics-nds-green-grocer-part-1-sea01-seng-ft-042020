7debf726f373e704439c9944b9ed959b95455bd5
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-12 19:23:02 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,7 +6,7 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   count = 0 
   collection.each do | item , index |
-    binding.pry
+    # binding.pry
     if collection[index][:item] == name 
       return collection[count]
     end
